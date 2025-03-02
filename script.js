b8e26160d1ddf20fce046bf02676a276c745b758
Jamshid <jamshidnt2024@gmail.com>
2025-03-03 02:57:27 +0500

    BookCard lar qushildi

--- script.js
+++ script.js
@@ -49,3 +49,35 @@ menuBar.onclick = function(){
 closeMenu.onclick = function(){
     menuBarOpen.classList.remove("goMiddle")
 }
+
+let mainCards = document.getElementById("mainCards");
+
+document.addEventListener("DOMContentLoaded", addCards);
+
+async function addCards() {
+
+    let response = await fetch("https://fakerapi.it/api/v2/books?_quantity=24")
+    let data = await response.json();
+    let books = data.data;
+    mainCards.innerHTML = "";
+    books.forEach(book => {
+        mainCards.innerHTML += `
+        <div class="mainCards" id="mainCards">
+            <div class="mainCardsImg">
+                <img src="https://picsum.photos/200/300?random=100" alt="Book Cover">
+            </div>
+            <div class="mainCardsTitle">
+                <p class="genre">${book.genre}</p>
+                <p class="name">${book.title}</p>
+                <p class="author">${book.author}</p>
+                <p class="cost">$39</p>
+            </div>
+            <div class="mainCardsButtons">
+                <button>Add to Cart</button>
+                <i class='bx bx-heart'></i>
+                <i class='bx bx-dots-vertical-rounded'></i>
+            </div>
+        </div>
+        `;
+    });
+}
